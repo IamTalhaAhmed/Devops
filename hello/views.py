4b79685bd1f99e8989505f6c1ddb3c2f49ebe0fe
Talha <talha.malik1357@gmail.com>
2022-12-02 15:14:03 +0500

    5 k

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('best of luckkk')
+    return HttpResponse('best of luckkkkk')
